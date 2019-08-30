ce381940d1ddfba591fa43bc562f6362b2f5d05e
furgot <frtsang40@gmail.com>
2019-08-30 13:25:44 -0700

    verb and places list

--- Madlib.py
+++ Madlib.py
@@ -1,15 +1,26 @@
 #shoutout to GeeksforGeeks for the list assist!
 list_nouns = list(input("Enter at least two plural nouns: ").split())
-while len(list_nouns) == 0:
+while (len(list_nouns) < 2):
     list_nouns = list(input("Enter at least two plural nouns again: ").split())
 
 list_jobs = list(input("Enter at least three jobs: ").split())
-while len(list_jobs) == 0:
+while len(list_jobs) < 3:
     list_jobs = list(input("Enter at least three jobs again: ").split())
 
 list_animals =  list(input("Enter at least three animals: ").split())
-while len(list_animals) == 0:
+while len(list_animals) < 3:
     list_animals =  list(input("Enter at least three animals again: ").split())
+
+list_place = list(input("Enter at least two places: ").split())
+while len(list_place) < 2:
+    list_place = list(input("Enter at least two places again: ").split())
+
+list_verb = list(input("Enter at least three verbs: ").split()) 
+while len(list_verb) < 3:
+    list_verb = list(input("Enter at least three verbs again: ").split())
+
 print(list_nouns)
 print(list_jobs)
 print(list_animals)
+print(list_place)
+print(list_verb)
