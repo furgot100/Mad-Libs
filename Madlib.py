e84d9168550280801aec24c8ad7b4b5de53dd89b
furgot <frtsang40@gmail.com>
2019-08-30 11:11:18 -0700

    jobs list

--- Madlib.py
+++ Madlib.py
@@ -2,5 +2,8 @@
 list_noun = list(input("Enter at least two plural nouns: ").split())
 while len(list_noun) == 0:
     list_noun = list(input("Enter at least two plural nouns again: ").split())
-
+list_jobs = list(input("Enter at least three jobs: ").split())
+while len(list_jobs) == 0:
+    list_jobs = list(input("Enter at least three jobs again: ").split())
+print(list_jobs)
 print(list_noun)
