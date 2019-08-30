f5c6db2ffe12e11e43ecd3b2e6ea345d2dd29666
furgot <frtsang40@gmail.com>
2019-08-30 11:29:45 -0700

    animal list

--- Madlib.py
+++ Madlib.py
@@ -1,9 +1,15 @@
 #shoutout to GeeksforGeeks for the list assist!
-list_noun = list(input("Enter at least two plural nouns: ").split())
-while len(list_noun) == 0:
-    list_noun = list(input("Enter at least two plural nouns again: ").split())
+list_nouns = list(input("Enter at least two plural nouns: ").split())
+while len(list_nouns) == 0:
+    list_nouns = list(input("Enter at least two plural nouns again: ").split())
+
 list_jobs = list(input("Enter at least three jobs: ").split())
 while len(list_jobs) == 0:
     list_jobs = list(input("Enter at least three jobs again: ").split())
+
+list_animals =  list(input("Enter at least three animals: ").split())
+while len(list_animals) == 0:
+    list_animals =  list(input("Enter at least three animals again: ").split())
+print(list_nouns)
 print(list_jobs)
-print(list_noun)
+print(list_animals)
